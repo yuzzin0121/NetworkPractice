5690fdb1500c6773c1f264863bf4600d4a1db37c
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-01-16 16:12:41 +0900

    Initial Commit

---
+++ NetworkPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkPractice
+//
+//  Created by 조유진 on 1/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
